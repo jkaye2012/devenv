f2838cff1e4a2bc1abd71e9f0d5ca7c1abce710c
Jordan Kaye <jkaye@belvederetrading.com>
2025-05-08 14:33:20 -0600

    Adds global just commands

--- bash-aliases.nix
+++ bash-aliases.nix
@@ -1,6 +1,7 @@
 {
   ghostty = "nixGLIntel ghostty";
   j = "just";
+  jg = "just --justfile ${./justfile} --working-directory .";
   lz = "lazygit";
   z = "zellij";
 }
---
+++ justfile
@@ -0,0 +1,4 @@
+
+bt-login:
+    gitpod login --host https://belvedere.gitpod.cloud
+    glab auth login
